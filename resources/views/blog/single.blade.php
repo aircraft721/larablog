3c64b1537d6a07c8a10b5782d759c1bcf8f9c30c
Dan Constantinescu <dan_constantinescu@outlook.com>
2017-01-18 13:22:42 +0200

    Added disqus to the single.blade.php

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -17,8 +17,28 @@
                 <div class="tags-post">Posted In:  </div>
                 <img src="css/witcher.png" alt="">
                 <p>{{ $post->body }}</p>
-
             </div>
+
+        <div id="disqus_thread"></div>
+        <script>
+
+            /**
+             *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+             *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+
+            var disqus_config = function () {
+                this.page.url = www.blogf.dev;  // Replace PAGE_URL with your page's canonical URL variable
+                this.page.identifier = aircraft72; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+            };
+
+            (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = '//aircraft72.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 
 
     </div>
@@ -38,4 +58,7 @@
     </div>
 </div>
 
-    @stop
+
+
+@stop
+
