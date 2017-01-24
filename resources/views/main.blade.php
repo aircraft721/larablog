c925158af4511dca1d053c8f32e71c99abba79a7
Dan Constantinescu <dan_constantinescu@outlook.com>
2017-01-24 13:49:36 +0200

    Smooth scroll and google analytics

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -90,11 +90,15 @@
 
 
 
-@include('partials.analytics')
+
+
+
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+{!! Html::script('js/SmoothScroll.js') !!}
+{!! Html::script('js/analytics.js') !!}
 {!! Html::script('js/parsley.min.js') !!}
 @yield('script')
 
